ee4daad9719bae0a3db838cf89e5101337bacf00
Guilhermer Gonçalves <guilherme16.gon@gmail.com>
2022-09-27 14:18:53 -0300

    Seção5:Django URLs, Views e Templates

--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,7 @@ from django.shortcuts import render
 
 def home(request: HttpRequest) -> HttpResponse:
     
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Guilherme',
     })
 
-def contato(request: HttpRequest) -> HttpResponse:
-
-    return HttpResponse("<h2>Página de contatos</h2>")
-
-def sobre(request: HttpRequest) -> HttpResponse:
-
-    return HttpResponse("<h2>Página sobre</h2>")
