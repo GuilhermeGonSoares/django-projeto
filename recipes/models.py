45f8f5c52d35e4659d9438c9e4f967d8a7f746d2
Guilhermer Gonçalves <guilherme16.gon@gmail.com>
2022-09-28 14:02:29 -0300

    Seção8: Começando com ORM

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,15 @@
-
+from django.contrib.auth.models import User
 from django.db import models
 
 
+#One category have Many recipe
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -16,4 +24,20 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
 
+
+    def __str__(self) -> str:
+        return self.title
+
+
+
+
+
+ 
